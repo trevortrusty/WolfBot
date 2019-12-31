0cac08d45d877dc252240bd1ec6d2af5fe1fe77a
Trevor N Trusty <tsquared21@outlook.com>
2019-12-31 17:23:42 -0500

    Wolfram exports output as image, and sends back to discord context

--- bot_entry.py
+++ bot_entry.py
@@ -1,4 +1,4 @@
-##msg = await client.wait_for('message', check=check)
+#!/usr/bin/python3
 
 import discord
 import os
@@ -8,17 +8,27 @@ from discord.utils import get
 
 from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wl, wlexpr
+from wolframclient.evaluation import SecuredAuthenticationKey, WolframCloudSession
+from wolframclient.exception import WolframEvaluationException
 from PIL import Image
 
+# Is wolf bot running in server already?
+inuse = False
+
+# Authentication Key
+sak = SecuredAuthenticationKey(
+
+    't9JwbVPeUiX0G38n8/GtrLcz0S1vXrTiNfvHbNFl5U0=',
+
+    'dbbLmj8rDSrTEe0A+baPcMIFgFhmVPfFMdLg4trDuFc=')
+#
 
 def enlarge():
-    img = Image.open('D:/dev/discordbots/WolfBot/output/output.png', 'r')
+    img = Image.open('D:/dev/discordbots/WolfBot/output/output.jpg', 'r')
     img_w, img_h = img.size
     if img_w < 100 and img_h < 40:
         background = Image.new('RGBA', (100, 40), (255, 255, 255, 255))
         bg_w, bg_h = background.size
-        # offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-        # background.paste(img, offset)
         background.paste(img,(15,12))
         background.save('D:/dev/discordbots/WolfBot/output/output.png')
 
@@ -31,34 +41,48 @@ async def on_ready():
 
 
 @client.command()
-async def wolf(ctx):
-    channel = ctx.message.channel
-    def check(m):
-        return m.channel == channel
+async def wolf(ctx, inuse=inuse):
+    if not inuse:
+        inuse = True
+        channel = ctx.message.channel
+        def check(m):
+            return m.channel == channel
 
-    session = WolframLanguageSession('D:\\Program Files\\Wolfram Research\\Wolfram Engine\\12.0\\WolframKernel.exe')
-    
-    begin = r'Export["D:\\dev\\discordbots\\WolfBot\\output\\output.png",'
-    end = ']'
-    
-    counter = 0
-    await ctx.send(f'```In[{counter}]:=    ```')
-    msg = await client.wait_for('message', check = check)
-    export = begin + msg.content + end
-    while msg.content != 'exit' :
-        if msg.content != 'exit':
-            async with ctx.typing():
-                session.evaluate(export)
-                enlarge()
-                await ctx.send(file=discord.File('D:\dev\discordbots\WolfBot\output\output.png'))
-            #await ctx.send(f'```Out[{counter}]:= {(session.evaluate(wlexpr(msg.content)))}```')
-            counter = counter + 1
-            await ctx.send(f'```In[{counter}]:=    ```')
-            msg = await client.wait_for('message', check = check)
-            export = begin + msg.content + end
-    session.terminate()
-    await ctx.send('>>> Wolfram session terminated!')
+        session = WolframLanguageSession('D:\\Program Files\\Wolfram Research\\Wolfram Engine\\12.0\\WolframKernel.exe')
 
+        session.start()
+        begin = 'Export["D:/dev/discordbots/WolfBot/output/output.jpg",'
+        end = ']'
+        
+        counter = 0
+        await ctx.send(f'```In[{counter}]:=    ```')
+        msg = await client.wait_for('message', check = check)
+        export = begin + msg.content + end
+        while msg.content != 'exit' :
+            if msg.content != 'exit':
+                try:
+                    async with ctx.typing():
+                        session.evaluate(wlexpr(export))
+                        enlarge()
+                        await ctx.send(file=discord.File('D:/dev/discordbots/WolfBot/output/output.png'))
+                    counter = counter + 1
+                    await ctx.send(f'```In[{counter}]:=    ```')
+                    msg = await client.wait_for('message', check = check)
+                    export = begin + msg.content + end
+                except WolframEvaluationException as err:
+                    await ctx.send('Evaluation error: ', err)
+        inuse = False
+        session.terminate()
+
+        # Send Embed message for end of session #
+        embed = discord.Embed(
+            title = f'**Wolfram session terminated!**', 
+            color = discord.Color.blue(), 
+            description = f'Session started by\n{ctx.message.author.mention}')
+        await ctx.send(embed = embed)
+        # await ctx.send('>>> Wolfram session terminated!')
+    else:
+        await ctx.send('>>> WolfBot already in use in another channel')
 client.run('NjUzODA3MTM3NjkyMTg4Njcy.Xe8Xlg.-EDzSXrTejAAuJ2sCI-0mfwUxjY')
 
 
