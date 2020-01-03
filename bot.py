57be9988689b943b665bc068003be81c9ff645ed
Trevor N Trusty <tsquared21@outlook.com>
2020-01-03 16:41:51 -0500

    Made seperate bot versions with and without session command
    Also removed extra garbage scripts

--- bot.py
+++ bot.py
@@ -51,82 +51,8 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
-@client.command()
-@commands.has_any_role('Admin', 'Bot Henchmen', 'Development Team')
-async def session(ctx):
-
-    async with ctx.typing():
-        channel = ctx.message.channel
-        def check(m):
-            return m.channel == channel and ((m.content).startswith('wl ') or (m.content).startswith('```wl ')) and m.author == ctx.message.author
-
-    async with WolframLanguageAsyncSession(kernel_path) as session:
-
-        async with ctx.typing():
-            # Start Asynchronous Wolfram Kernel thread #
-            await session.start()
-            # Tell user that the session has successfully started
-            start_alert = discord.Embed(
-                title = f'**Wolfram Session Started!**',
-                color = discord.Color.blue(),
-                description = f'Initiated by\n{ctx.message.author.mention}')
-            await ctx.send(embed = start_alert)
-
-        # Prepares the user input to be passed into Wolfram functions that export the output image, and limit the time of the computation 
-        begin = f'Export["{img_path}", TimeConstrained['
-        end = ', 60, "Your computation has exceeded one minute."]]'
-
-        # Wait for discord user to enter input, and save message to msg.
-        msg = await client.wait_for('message', check = check)
-
-        # save string from message object to a string variable
-        wolfcommand = msg.content 
-        if wolfcommand.startswith('wl '):
-            wolfcommand = wolfcommand.replace('wl ', '')
-        elif wolfcommand.startswith('```wl '):
-            wolfcommand = wolfcommand.replace('```', '')
-            wolfcommand = wolfcommand.replace('wl ', '')
-        #wolfcommand = wolfcommand.replace('wl ', '')
-
-        # concatenate the full command for passing to Wolfram
-        export = begin + wolfcommand + end 
-
-
-        # Loop session, sending output from initial input, taking in new input, repeat.
-        while msg.content != 'wl exit' and msg.content != '```wl exit' :
-            if msg.content != 'wl exit' and msg.content != '```wl exit':
-                try:
-                    async with ctx.typing():
-                        await session.evaluate(wlexpr(export))
-                        enlarge()
-                        
-                        # Send image from Wolfram calculation results
-                        await ctx.send(file=discord.File(img_path))
-                    
-                    # Wait for new input from user
-                    msg = await client.wait_for('message', check = check)
-                    wolfcommand = msg.content 
-                    if wolfcommand.startswith('wl '):
-                        wolfcommand = wolfcommand.replace('wl ', '')
-                    elif wolfcommand.startswith('```wl '):
-                        wolfcommand = wolfcommand.replace('```', '')
-                        wolfcommand = wolfcommand.replace('wl ', '')
-                    export = begin + wolfcommand + end
-                except WolframLanguageException as err:
-                    error = err
-                    await ctx.send(error)
-
-        # Loop seninent value detected, closes connection to wolfram kernel
-        await session.stop()
-
-    # Send Embed message for end of session #
-    end_message = discord.Embed(
-        title = f'**Wolfram session terminated!**',
-        color = discord.Color.blue(),
-        description = f'Session started by\n{ctx.message.author.mention}')
-    await ctx.send(embed = end_message)
-
+session = WolframLanguageAsyncSession(kernel_path)
+session.start()
 
 @client.command()
 @commands.has_any_role('Admin', 'Bot Henchmen', 'Development Team')
@@ -137,45 +63,44 @@ async def bark(ctx,*, script):
         end = ', Large], 60, "Your computation has exceeded one minute."]]'
         export = begin + script + end
 
-        async with WolframLanguageAsyncSession(kernel_path) as session:
-            #await session.start()
-            try:
-                eval = await session.evaluate_wrap(wlexpr(export), timeout= 5)
+        #await session.start()
+        try:
+            eval = await session.evaluate_wrap(wlexpr(export), timeout= 5)
 
-                # Check for errors before sending result
-                log = str(eval.messages)
+            # Check for errors before sending result
+            log = str(eval.messages)
 
-                if len(log) > 256:
-                    #log = log[0 : 255 : 1] # Shorten length of string to 255 characters to satify discord embed char limit
-                    log = 'Errors were detected during computation'
-                if log != 'None':
-                    if (log).startswith('(\'Invalid syntax'):
-                        log = createEmbed('Invalid syntax!')
-                        await ctx.send(embed = log)
-                    else:
-                        log = createEmbed(log)
-                        enlarge()
-                        await ctx.send(file=discord.File(img_path))
-                        await ctx.send(embed = log) 
+            if len(log) > 256:
+                #log = log[0 : 255 : 1] # Shorten length of string to 255 characters to satify discord embed char limit
+                log = 'Errors were detected during computation'
+            if log != 'None':
+                if (log).startswith('(\'Invalid syntax'):
+                    log = createEmbed('Invalid syntax!')
+                    await ctx.send(embed = log)
                 else:
-                    # No errors, continue
+                    log = createEmbed(log)
                     enlarge()
-                    
-                    # Send image from Wolfram calculation results
                     await ctx.send(file=discord.File(img_path))
-            except TimeoutError:
-                log = createEmbed('Timeout Error: Computation took too long!')
-                await ctx.send(embed = log)
+                    await ctx.send(embed = log) 
+            else:
+                # No errors, continue
+                enlarge()
+                
+                # Send image from Wolfram calculation results
+                await ctx.send(file=discord.File(img_path))
+        except TimeoutError:
+            log = createEmbed('Timeout Error: Computation took too long!')
+            await ctx.send(embed = log)
 
-            # Send Goodbye Embed message
-            end_message = discord.Embed(
-                title = f'**Learn more the about Wolfram Language**',
-                color = discord.Color.blue(),
-                description = f'Requested by\n{ctx.message.author.mention}',
-                url = 'https://reference.wolfram.com/language/')
-            end_message.set_thumbnail(url = 'https://media1.tenor.com/images/ed4da9a1bdbd4ff952638b19afa96506/tenor.gif?itemid=12660466')
-            await ctx.send(embed = end_message)
-            #await session.stop()
+        # Send Goodbye Embed message
+        end_message = discord.Embed(
+            title = f'**Learn more the about Wolfram Language**',
+            color = discord.Color.blue(),
+            description = f'Requested by\n{ctx.message.author.mention}',
+            url = 'https://reference.wolfram.com/language/')
+        end_message.set_thumbnail(url = 'https://media1.tenor.com/images/ed4da9a1bdbd4ff952638b19afa96506/tenor.gif?itemid=12660466')
+        await ctx.send(embed = end_message)
+        #await session.stop()
 
 client.run('NjUzODA3MTM3NjkyMTg4Njcy.Xe8Xlg.-EDzSXrTejAAuJ2sCI-0mfwUxjY')
 
--- wsession.py
+++ wsession.py
@@ -6,11 +6,12 @@ from discord.ext import commands, tasks
 from collections import defaultdict
 from discord.utils import get
 
-from wolframclient.evaluation import WolframLanguageSession, WolframLanguageAsyncSession, WolframEvaluatorPool
+from wolframclient.evaluation import WolframLanguageSession, WolframLanguageAsyncSession
 from wolframclient.language import wl, wlexpr
 from wolframclient.evaluation import SecuredAuthenticationKey, WolframCloudSession
 from wolframclient.exception import WolframEvaluationException, WolframLanguageException, WolframKernelException
 from PIL import Image
+import PIL.ImageOps 
 
 #Define paths
 img_path = 'D:/dev/discordbots/WolfBot/output/output.jpg'
@@ -34,7 +35,8 @@ def enlarge():
     background = Image.new('RGB', (img_w + 25, img_h + 25), (255, 255, 255, 255))
     bg_w, bg_h = background.size
     background.paste(img,(13,12))
-    background.save(img_path)
+    final = PIL.ImageOps.invert(background)
+    final.save(img_path)
 
 # Creates a discord.Embed object #
 def createEmbed(t):
@@ -44,9 +46,6 @@ def createEmbed(t):
     return embed
 
 client = commands.Bot(command_prefix = '$')
-
-session = WolframEvaluatorPool(kernel_path)
-session.start()
 
 @client.event
 async def on_ready():
@@ -61,8 +60,9 @@ async def session(ctx):
         channel = ctx.message.channel
         def check(m):
             return m.channel == channel and ((m.content).startswith('wl ') or (m.content).startswith('```wl ')) and m.author == ctx.message.author
-    
+
     async with WolframLanguageAsyncSession(kernel_path) as session:
+
         async with ctx.typing():
             # Start Asynchronous Wolfram Kernel thread #
             await session.start()
@@ -133,59 +133,49 @@ async def session(ctx):
 async def bark(ctx,*, script):
     # Prepares the user input to be passed into Wolfram functions that export the output image, and limit the time of the computation 
     async with ctx.typing():
-        # begin = f'Export["{img_path}", TimeConstrained['
-        # end = ', 60, "Your computation has exceeded one minute."]]'
-        # export = begin + script + end
-
-        begin = f'Export["{img_path}", '
-        end = ']'
+        begin = f'Export["{img_path}", TimeConstrained[Style['
+        end = ', Large], 60, "Your computation has exceeded one minute."]]'
         export = begin + script + end
 
-        #await session.start()
-        try:
-            eval = await session.evaluate_wrap(wlexpr(export), timeout= 5)
-            await session.evaluate('ClearAll')
-            # Check for errors before sending result
-            log = str(eval.messages)
+        async with WolframLanguageAsyncSession(kernel_path) as session:
+            #await session.start()
+            try:
+                eval = await session.evaluate_wrap(wlexpr(export), timeout= 5)
 
-            if len(log) > 256:
-                #log = log[0 : 255 : 1] # Shorten length of string to 255 characters to satify discord embed char limit
-                log = 'Errors were detected during computation'
-            if log != 'None':
-                if (log).startswith('(\'Invalid syntax'):
-                    log = createEmbed('Invalid syntax!')
-                    await ctx.send(embed = log)
+                # Check for errors before sending result
+                log = str(eval.messages)
+
+                if len(log) > 256:
+                    #log = log[0 : 255 : 1] # Shorten length of string to 255 characters to satify discord embed char limit
+                    log = 'Errors were detected during computation'
+                if log != 'None':
+                    if (log).startswith('(\'Invalid syntax'):
+                        log = createEmbed('Invalid syntax!')
+                        await ctx.send(embed = log)
+                    else:
+                        log = createEmbed(log)
+                        enlarge()
+                        await ctx.send(file=discord.File(img_path))
+                        await ctx.send(embed = log) 
                 else:
-                    log = createEmbed(log)
+                    # No errors, continue
                     enlarge()
+                    
+                    # Send image from Wolfram calculation results
                     await ctx.send(file=discord.File(img_path))
-                    await ctx.send(embed = log) 
-            else:
-                # No errors, continue
-                enlarge()
-                
-                # Send image from Wolfram calculation results
-                await ctx.send(file=discord.File(img_path))
-        except TimeoutError:
-            log = createEmbed('Timeout Error: Computation took too long!')
-            await ctx.send(embed = log)
+            except TimeoutError:
+                log = createEmbed('Timeout Error: Computation took too long!')
+                await ctx.send(embed = log)
 
-        # Send Goodbye Embed message
-        end_message = discord.Embed(
-            title = f'**Learn more the about Wolfram Language**',
-            color = discord.Color.blue(),
-            description = f'Requested by\n{ctx.message.author.mention}',
-            url = 'https://reference.wolfram.com/language/')
-        end_message.set_thumbnail(url = 'https://media1.tenor.com/images/ed4da9a1bdbd4ff952638b19afa96506/tenor.gif?itemid=12660466')
-        await ctx.send(embed = end_message)
-        #await session.stop()
-
-
-@client.command()
-@commands.has_any_role('Admin', 'Bot Henchmen', 'Development Team')
-async def stop(ctx):
-    session.stop()
-
+            # Send Goodbye Embed message
+            end_message = discord.Embed(
+                title = f'**Learn more the about Wolfram Language**',
+                color = discord.Color.blue(),
+                description = f'Requested by\n{ctx.message.author.mention}',
+                url = 'https://reference.wolfram.com/language/')
+            end_message.set_thumbnail(url = 'https://media1.tenor.com/images/ed4da9a1bdbd4ff952638b19afa96506/tenor.gif?itemid=12660466')
+            await ctx.send(embed = end_message)
+            #await session.stop()
 
 client.run('NjUzODA3MTM3NjkyMTg4Njcy.Xe8Xlg.-EDzSXrTejAAuJ2sCI-0mfwUxjY')
 
