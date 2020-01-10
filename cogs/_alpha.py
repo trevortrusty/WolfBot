ce295a1d36a1817c58542e210c710a929ad32123
Trevor N Trusty <tsquared21@outlook.com>
2020-01-10 16:19:48 -0500

    fixed paths.py import in _alpha

--- cogs/_alpha.py
+++ cogs/_alpha.py
@@ -13,7 +13,7 @@ import PIL.ImageOps
 import asyncio
 import embeds
 from  cogs._bark import session
-from cogs.paths import img_path, kernel_path
+from paths import img_path, kernel_path
 #Define paths
 # img_path = 'D:/dev/discordbots/WolfBot/output/output.jpg'
 #img_path = '/home/pi/WolfBot/output/output.jpg'
