3f925ce8152c8b1fd22c3f50b8bf46ed474408c3
Trevor N Trusty <tsquared21@outlook.com>
2020-01-10 16:55:12 -0500

    Ping message color changes with client latency

--- cogs/_bark.py
+++ cogs/_bark.py
@@ -102,9 +102,19 @@ class Bark(commands.Cog):
     # Ping
     @commands.command()
     async def ping(self, ctx):
+        bot_latency = round(self.client.latency * 1000)
+        if bot_latency <= 50:
+            meter = discord.Color.green()
+        elif bot_latency < 100:
+            meter = discord.Color.gold()
+        elif bot_latency >= 100:
+            meter  = discord.Color.red()
+        elif bot_latency >= 500:
+            meter = discord.Color.dark_grey()
+
         ping_embed = discord.Embed(
-            title = f'Woof! {round(self.client.latency *1000)} ms',
-            color = discord.Color.gold()
+            title = f'Woof! {bot_latency} ms',
+            color = meter
         )
         ping_embed.set_footer(text = f'requested by {ctx.message.author}', icon_url=ctx.message.author.avatar_url)
         await ctx.send(embed = ping_embed)
