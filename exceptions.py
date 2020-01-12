dfb0bcc05fb791f9d63eb5573778012d01bdc70e
Trevor N Trusty <tsquared21@outlook.com>
2020-01-12 14:20:22 -0500

    Added server link to error messages

--- exceptions.py
+++ exceptions.py
@@ -3,13 +3,13 @@
 class WhiteListError(Exception):
     def __init__(self, bad = 0):
         if bad:
-            self.message = f'{bad} is not currently in our list of allowed functions! Please contact the dev team to have it added'
+            self.message = f'{bad} is not currently in our list of allowed functions! Please contact the dev team to have it added by joining our support server: https://discord.gg/eyd376A'
         else:
             self.message = 'This function is not currently allowed! Please contact the dev team to have it added'
 
 class BlackListError(Exception):
     def __init__(self, bad = 0):
         if bad:
-            self.message = f'\'{bad}\' is a banned keyword/function for WolfBot. Contact my dev team if you think this is a mistake'
+            self.message = f'\'{bad}\' is a banned keyword/function for WolfBot. Contact the dev team at https://discord.gg/eyd376A if you think this is a mistake'
         else:
             self.message = 'You\ve typed a banned phrase, so we cannot evaluate this code right now!'
