1792cad12ee5ec6b492578d1d6c7252e78a4c51e
Trevor N Trusty <tsquared21@outlook.com>
2020-01-12 00:06:38 -0500

    update bark.py

--- functions.py
+++ functions.py
@@ -15,31 +15,15 @@ import embeds
 from  cogs.bark import session
 from paths import img_path, kernel_path
 
-
-async def eval_input(script):
-    export = f'Export["{img_path}", Style[{script}, Large]]'
-
-    # Evaluate given expression, exporting result as png
-    eval = await asyncio.wait_for(session.evaluate_wrap(wlexpr(export)), 40)
-
-    # Check for errors before sending result
-    log = str(eval.messages)
-    return log
-
-async def send_error(ctx, log):
-    if log != 'None':
-        if(len(log) > 256):
-            await ctx.send(embed = embeds.general_error)
-        elif (log).startswith('(\'Invalid syntax'):
-            await ctx.send(embed = embeds.syntax_error)
-        elif log.startswith('(\'Not enough memory available to rasterize Notebook expression.\',)'):
-            await ctx.send(embed = embeds.memory_error)
-            await ctx.send(f'```{await session.evaluate_wrap(wlexpr(script), timeout = 5)}```')
+class WhiteListError(Exception):
+    def __init__(self, bad = 0):
+        if bad:
+            self.message = f'{bad} is not currently in our list of allowed functions! Please contact the dev team to have it added'
         else:
-            log = embeds.createEmbed(log)
-            enlarge()
-            await ctx.send(file=discord.File(img_path))
-            await ctx.send(embed = log) 
-        return 1
-    else:
-        return 0
+            self.message = 'This function is not currently allowed! Please contact the dev team to have it added'
+
+def wrap_wolf(s):
+    if s == "bad":
+        raise WhiteListError
+    wrapped f'Export["{img_path}", Style[{s}, Large], Background -> None, ImageResolution -> 100]'
+    return wrapped
