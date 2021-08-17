2bdbd00d2de42ad00fc9837991e985120883b902
trevortrusty <trevortrusty@outlook.com>
2021-08-17 16:54:50 -0400

    Remove regex method for whitelisting input, rewrite bark command to vet the input through a user-defined wolfram function to make sure user's can't use wolfram functions they shouldn't be using

--- functions.py
+++ functions.py
@@ -19,35 +19,10 @@ import re
 import exceptions
 
 def wrap_wolf(s):
-    with open(wl_path, 'r') as f:
-        reader = csv.reader(f)
-        whitelist = list(reader)[0]
-    with open(bl_path, 'r') as f:
-        reader = csv.reader(f)
-        blacklist = list(reader)[0]    
-
-
-    pattern = r"([A-Z]{1,2}[a-z]+'*)\[.*?"
-
-    functions = re.findall(pattern, s)
-
-    allow = True
-    for function in functions:
-        if function not in whitelist:
-            allow = False
-            raise exceptions.WhiteListError(function)
-            break
-    
-    for phrase in blacklist:
-        if phrase in s:
-            raise exceptions.BlackListError(phrase)
-            break
-
     if 'Animate' not in s:
-        return f'Export["{img_path}", Style[{s}, Large, FontFamily -> "Cambria"], Background -> None]'
+        return f'Export["{img_path}", Style[whit[{s}], Large, FontFamily -> "Cambria"], Background -> None]'
     else:
-        return f'Export["{file}/output/output.gif", {s}]'
-    #, ImageResolution -> 100
+        return f'Export["{file}/output/output.gif", whit[{s}]]'
 
 # Enlarges image output from Wolfram calculation, and then saves as png #
 async def enlarge(ctx):
